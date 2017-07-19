39dad41e4a7105483339a1d434c1e9fd35416d1b
JunePaloma <kborkoski@gmail.com>
2017-07-19 09:13:29 -0600

    add instance of test

--- lib/joke.rb
+++ lib/joke.rb
@@ -1,3 +1,13 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/joke'
+require './lib/user'
+require "pry"
+
 class Joke
+
+  def test_theres_a_joke
+    assert_instance_of Joke, Joke.new
+  end
 
 end
